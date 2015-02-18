0ec0a463db2cd7a0c544473c904f5071699a479c
Arsh Chauhan <achauhan@alaska.edu>
2015-02-17 17:03:04 -0900

    Just gotta get it to print in master

--- Assignment2/delegate.cpp
+++ Assignment2/delegate.cpp
@@ -1,3 +1,11 @@
+/*
+	Arsh Chauhan
+	CS321 Assignment 2
+	delegate.cpp: Multithreaded Implementation for sa2a.h
+	Modified: 2/17/2015
+
+*/
+
 
 #include<iostream>
 #include<thread>
@@ -12,25 +20,32 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::mutex;
+using std::lock_guard;
 
 vector<thread> workers(6);
 vector<unsigned int> sequence;
 vector<int> results;
 
 
-int iteration = 0;
+int iteration = 1;
+int totalIterations;
+bool print = false;
 
 mutex shared;
 
 void run(int n )
 {
-	while(iteration<workers.size()-1)
+	lock_guard<mutex> count_guard(shared);{
+	while(iteration!=totalIterations)
 	{
-		shared.lock();
-		results.push_back(sa2a(sequence[iteration]));
-		iteration+=1;
-		shared.unlock();
-	}
+		{
+			
+			results[iteration-1] = (sa2a(sequence[iteration-1]));
+			cout <<"sa2a (" << sequence[iteration-1] <<") = "<<results[iteration-1]<<endl;;
+			if(iteration<totalIterations)iteration++;
+			print = true;
+		}
+	}}
 
 }
 
@@ -38,19 +53,24 @@ int main()
 {
 	
 	
-	unsigned int lastNum;
+	int lastNum;
 	
 	do 
 	{
+		
 		cout << "Enter a positive integer (or 0 to end input) : "; 
 		cin >> lastNum;
 		cout << endl;
 		if(lastNum >0 )
 			sequence.push_back(lastNum);
+		if(lastNum<0)
+			cout<<" Only Positive numbers allowed " << endl;
 	}
-	while(lastNum>0); //FIXME: Handle Negative values 
-
-	for(int i = 0;i < workers.size(); i++)
+	while(lastNum!=0); 
+	//cout <<"Size of seqence :"<<sequence.size();
+	totalIterations = sequence.size();
+	results.resize(totalIterations);
+	for(int i = 1;i < workers.size(); i++)
 	{
 		try
 		{
@@ -63,17 +83,23 @@ int main()
 			exit(1);
 		}
 	}
-	while(iteration<workers.size()-1)
+	static int outputsPrinted = 0;
+	lock_guard<mutex> count_guard(shared);{
+	while(outputsPrinted!=totalIterations){
+	if(print)
 	{
-		shared.lock();
-		if(results.size()!=0)
-		{
-			cout <<"sa2a (" << sequence[iteration] <<") = "<<results[iteration];
-		}
-		results.pop_back();
-		shared.unlock();
-	}
-	for(int i = 0; i<workers.size()-1; i++)
+		
+		
+		
+			{
+				cout <<"sa2a (" << sequence[iteration-1] <<") = "<<results[iteration-1];
+				print = false;
+				outputsPrinted++;
+			}
+			
+		}}}
+	
+	for(int i = 1; i<workers.size(); i++)
 	{
 		workers[i].join();
 	}
