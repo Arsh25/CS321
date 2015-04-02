a98a51a1b05e35385eabf2e451c54543ded6c9e1
Arsh Chauhan <achauhan@alaska.edu>
2015-04-01 23:02:01 -0800

    if mmap fails, return nullptr

--- Assignment4/mapalloc.cpp
+++ Assignment4/mapalloc.cpp
@@ -1,31 +1,16 @@
 #include "mapalloc.h"
-#include<iostream>
-#include<vector>
+
+
 #include<map> // for std::map
 
 using std::size_t;
 using std::map;
 
 
-
-/*struct memoryAllocated
-{
-	void * _startAddress;
-	size_t _size;
-};
-
-using std::vector;*/
-
-
-
-
-int count = 0;
-//vector <memoryAllocated> memoryMapped;
-//vector<memoryAllocated>::iterator it;
 map<void *, size_t> memoryMapped;
 
 
-void * Freeaddr = nullptr;
+void * Freeaddr = nullptr; // Starting Address of next free memory block
 
 
 
@@ -38,43 +23,20 @@ void *mapalloc( const size_t size)
 	}
 	else
 	{ 
-		Freeaddr = (void *)((char *)Freeaddr + size);
+		Freeaddr = (void *)((char *)Freeaddr + size); // Ofset the next free address pointer by 'size' bytes
 		memoryMapped[addr] = size;
 		return addr;
 	}
-	
-	
-	
-	
 }
 
 
 
-void mapFree( void *block) // Change the memory size
+void mapFree( void *block) 
 {
 	
 	 int success = munmap( block, memoryMapped[block]);
-	 if(!success) std::cout << "Deallocation Succeeded" <<std::endl;
+
 }
 
-//Function to easily run multiple test cases. Only for debugging  
-void test(size_t bytes)
-{
-	std::cout<<"Allocating Memory"<<std::endl;
-	void * mem = mapalloc(bytes);
-	if(mem !=nullptr)
-	{
-		std::cout<<"mmap succeeded"<<std::endl;
-		std::cout<<"Starting Address ="<<Freeaddr<<std::endl;
-	}
-	mapFree(mem);
-}
-int main()
-{
-	for(int i=0;i<100000;i++)
-	{
-		test(i);
-	}
-	return 0;
-}
+
 		
