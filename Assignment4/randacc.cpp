b25fc0b82890f315ddbb71a6c2da192ce5579331
Arsh Chauhan <achauhan@alaska.edu>
2015-04-02 12:50:26 -0800

    Fixed memorymapping error. Now displays stderr if an error is encountered. Still not reading or writing records correctly

--- Assignment4/randacc.cpp
+++ Assignment4/randacc.cpp
@@ -1,5 +1,11 @@
+/*Arsh Chauhan
+ * Last Modified: 04/02/2015
+ * randacc.cpp : random file access using mmap 
+*/
 
-#include<iostream> // for cout,cin, endl
+
+
+#include<iostream> // for cout, cin, endl
 using std::cout;
 using std::cin;
 using std::endl;
@@ -7,11 +13,15 @@ using std::endl;
 using std::size_t;
 #include<string>
 using std::string;
-#include<unistd.h> // for _exit,write,close
+#include<unistd.h> // for _exit, write, close
 #include<sys/mman.h> // for mmao, munmap, related constants
 #include<fcntl.h> // for open, related constants
 #include<sys/stat.h> // for stat, struct stat
 #include<sys/types.h> // for off_t
+
+//Possible debuggin includes only 
+#include<cerrno> // for errno 
+#include<string.h> // for strerror
 
 
 
@@ -21,23 +31,36 @@ off_t getFileSize(const string & filepath)
 	
 	int fileSize = stat(filepath.c_str(), &fileinfo);
 	
-	if(fileSize)
+	if(fileSize == -1)
 	{
 		return -1;
 	}
 	
+	cout <<"FileSize = " <<fileinfo.st_size << endl;
 	return fileinfo.st_size;
+	
 	
 }
 void closeFile(int fd)
 {
+	cout << "Closing File"<<endl;
+	int closeFailed = close(fd);
+	if(closeFailed)
+	{
+		cout <<" Failed to close file: exiting  " <<endl;
+		_exit(1);
+	}
+	else
+	{
+		cout<<" Close Suceeded " <<endl;
+	}
 }
 
 void readFile(string & filepath)
 {
 	int record;
 	off_t fileSize = getFileSize(filepath);
-	int fd = open(filepath.c_str(),O_CREAT|O_RDONLY,0644);
+	int fd = open(filepath.c_str(),O_CREAT|O_RDWR,0644);
 	if(fd == -1)
 	{
 		cout << "Could not open file" << filepath <<" exiting" <<endl;
@@ -53,6 +76,7 @@ void readFile(string & filepath)
 	if(addr == MAP_FAILED)
 	{
 		cout << "Failed to get memory to read file: exiting" <<endl;
+		cout << "errno = " << strerror(errno) << endl;;
 		closeFile(fd);
 		_exit(1);
 		
@@ -66,7 +90,33 @@ void readFile(string & filepath)
 
 void modifyFile(string & filepath)
 {
-	int fd = open(filepath.c_str(),O_CREAT|O_WRONLY,0644);
+	int fd = open(filepath.c_str(),O_CREAT|O_RDWR,0644);
+	if(fd == -1)
+	{
+		cout << "Could not open file" << filepath <<" exiting" <<endl;
+		_exit(1);
+	}
+	
+	void *addr = mmap(nullptr, getFileSize(filepath), PROT_WRITE|PROT_READ,MAP_SHARED,fd,0);
+	
+	if(addr == MAP_FAILED)
+	{
+		cout << "Failed to get memory : Exiting" << endl;
+		cout << "errno = " << strerror(errno) << endl;;
+		_exit(1);
+	}
+	int request;
+	int data;
+	cout << "Enter Record to modify (0-99) : " << endl;
+	cin >> request;
+	cout << "Enter integer to be written at position " <<request << " : " << endl;
+	cin >> data;
+	char * p = (char *)addr;
+	p[request] = data;
+	
+	closeFile(fd);
+	int munmapResult = munmap ( addr,getFileSize(filepath) );
+	
 }
 	
 void menu(string & filepath)
@@ -96,7 +146,7 @@ void menu(string & filepath)
 	
 int main()
 {
-	string filepath = "randacc.txt";
+	string filepath = "randacc.bin";
 	menu(filepath);
 	
 	
